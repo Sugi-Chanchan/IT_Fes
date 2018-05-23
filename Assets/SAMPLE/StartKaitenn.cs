8a1b6e3539f3097f51c2747045d6f18ca738a5f0
Sugi-Chan <sgnmssuioaai@gmail.com>
2018-05-23 19:00:51 +0900

    3Dモデル"Jakkun"追加 誰でも使えるようなスクリプトを追加

--- Assets/SAMPLE/StartKaitenn.cs
+++ Assets/SAMPLE/StartKaitenn.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rtation : MonoBehaviour {
+public class StartKaitenn : MonoBehaviour {
 
+    public int x;
+    public int y;
+    public int z;
 	// Use this for initialization
 	void Start () {
-		
+        transform.Rotate(new Vector3(x, y, z));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(0, 0.9f, 0));
+		
 	}
 }
