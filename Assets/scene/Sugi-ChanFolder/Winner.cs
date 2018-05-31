62068e3b098a4dd035cce70de04cc54faca38f63
Sugi-Chan <sgnmssuioaai@gmail.com>
2018-05-31 20:07:25 +0900

    爆発モーション

---
+++ Assets/scene/Sugi-ChanFolder/Winner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Winner : MonoBehaviour {
+
+    public GameObject explosion;
+    GameObject pl;
+	// Use this for initialization
+	void Start () {
+        Invoke("Win", 60);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Win()
+    {
+        SceneManager.LoadScene("SSSSample");
+
+
+       
+    }
+
+}
