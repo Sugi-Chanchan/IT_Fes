fa411103f0128a6cb94fb04cc39e270b854203fc
air <mturo5536@docomo.ne.jp>
2018-05-31 18:57:58 +0900

    RESULT変更

---
+++ Assets/iroiro/Scripts/Button_Result.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Button_Result : MonoBehaviour
+{
+
+    public void Push()
+    {
+        Bullet_easy.bullet_number = 1;
+        SceneManager.LoadScene("Start_air");
+    }
+
+}
